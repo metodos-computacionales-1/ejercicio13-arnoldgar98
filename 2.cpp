a9ff1bd9dfe6051c5397e788d2cfc291caf233d1
Default user <arnoldsgg@gmail.com>
2019-11-12 00:10:06 +0000

    ahora si

--- 2.cpp
+++ 2.cpp
@@ -16,5 +16,5 @@ int main(void)
     std::cout<< "The result is " <<z<<"\n";
     std::cout<< "The value of x is " <<x<<"\n";
     return 0;
-    //Si se agrega la palabra const, se produce un error, la palabra const vuelve a la variable una constante y por eso  sale el 
+    //Si se agrega la palabra const, se produce un error, la palabra const vuelve a la variable una constante y por eso  sale el error al intentar realizar la asignacion a=r
 }
